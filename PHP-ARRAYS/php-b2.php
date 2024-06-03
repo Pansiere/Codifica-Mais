4d083bd99af977199cf1433ceeea16672759bbe1
Joao Pedro V. Pansiere <ojp027@gmail.com>
2024-06-03 22:52:18 +0000

    Exercício php-b2.php

--- PHP-ARRAYS/php-b2.php
+++ PHP-ARRAYS/php-b2.php
@@ -8,7 +8,6 @@
     ● Porcentagem da gorjeta: 10%
     ● Valor da gorjeta: R$ 12,00
     ● Valor total a ser pago: R$ 132,00 */
-
     echo "Programa que calcula a gorjeta a ser paga em um restaurante. O solicita ao usuário o valor total da conta e a porcentagem da gorjeta. Em seguida, o programa calcula o valor da gorjeta e o valor total a ser pago, incluindo a gorjeta.\nDigite o valor total da conta: ";
 
     $valorDaConta = trim(fgets(STDIN));
