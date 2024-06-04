23a645a14297ef568df1a59b79996e926aefcdcf
Joao Pedro V. Pansiere <ojp027@gmail.com>
2024-06-04 23:14:01 +0000

    Update php-b1-15.php

--- PHP-ARRAYS/php-b1/php-b1-15.php
+++ PHP-ARRAYS/php-b1/php-b1-15.php
@@ -3,17 +3,22 @@
     Descrição: Escreva um programa que leia um array de 7 elementos e um número. O programa deve imprimir se o número está presente no array e em que posição(ões).
 */
     echo "Programa que lê um array de 7 elementos e um número. E imprimi se o número está presente no array e em que posição(ões).\n";
+    $encontrado = false;
 
     for ($i = 0; $i <= 6; $i++){
         echo $i == 0 ? "Digite o 1° elemento ou um número: " : "Digite o " . ($i + 1) . "° elemento ou um número: ";
         $lista[$i] = trim(fgets(STDIN));
+    }
+    echo "Digite um número para verificar se ele existe ou não na lista: ";
+    $numero = trim(fgets(STDIN));
 
-        if (is_numeric($lista[$i])){
-            $numerosPresentes[] = $lista[$i];
-            $indiceDosNumeros [] = $i;
+    for ($i = 0; $i < count($lista); $i++){
+        if($lista[$i] == $numero){
+            echo "O número $numero está presente na lista, e está no índice $i." . PHP_EOL;
+            $encontrado = true;
         }
     }
-    for ($i = 0; $i < count($numerosPresentes); $i++){
-        echo "No índice " .  $indiceDosNumeros[$i] . " existe um número, que é: " . $numerosPresentes[$i] . "." . PHP_EOL;
+    if ($encontrado != true){
+        echo "O número $numero não existe na lista.";
     }
 ?>
