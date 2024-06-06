60be662ea72d6fc8858ea279bd6ee8ceb79685dc
Joao Pedro V. Pansiere <ojp027@gmail.com>
2024-06-06 00:20:15 +0000

    Update php-b3.php

--- PHP-ARRAYS/php-b3.php
+++ PHP-ARRAYS/php-b3.php
@@ -9,12 +9,19 @@
     ● Largura: 5 metros
     ● Altura: 3 metros
     ● Área: 15 m²
-    ● Perímetro: 16 metros */
-    echo "Programa que calcula a área e o perímetro de um retângulo. O programa solicitará ao usuário a largura e a altura do retângulo, e então exibira os resultados.\nDigite a largura do retângulo: ";
+    ● Perímetro: 16 metros 
+    */
+    echo "Programa que calcula a área e o perímetro de um retângulo.\n";
+    echo "Digite a largura do retângulo: ";
     $largura = trim(fgets(STDIN));
+    $largura = floatval($largura);
+
     echo "Agora, digite a altura do retângulo: ";
     $altura = trim(fgets(STDIN));
+    $altura = floatval($altura);
+
     $area = $largura * $altura;
     $perimetro = 2 * ($largura + $altura);
-    echo "A área do retângulo é de: $area m², e o perímetro é de: $perimetro metros."
+
+    echo "A área do retângulo é de: " . $area . " m², e o perímetro é de: " . $perimetro . " metros.\n";
 ?>
