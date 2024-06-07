4b3dc647038b71a77cbd049cacd8bb6e01f5ec02
Joao Pedro V. Pansiere <ojp027@gmail.com>
2024-06-07 10:39:35 +0000

    Exercício php-b11

--- PHP-ARRAYS/php-b11.php
+++ PHP-ARRAYS/php-b11.php
@@ -1,37 +1,7 @@
 <?php
 
-//$estoque = [];
-
-$estoque[] = [
-    'codigo' => 1,
-    'nome' => 'Camisa Polo',
-    'tamanho' => 'P',
-    'cor' => 'Branca',
-    'quantidade' => 1
-];
-
-// Implemente aqui o código
-
-function listarEstoque($estoque){
-    if(empty($estoque)){
-        echo "Estoque vazio!\n";
-        return;
-    }
-    foreach($estoque as $produto){
-        echo "Código: " . $produto['codigo'] . 
-             ", nome: " . $produto['nome'] . 
-             " possui um total de " . $produto['quantidade'] .  " unidades no estoque.\n";
-    }
-}
-
-function adicionarProduto(&$estoque, $codigo, $nome, $tamanho, $cor, $quantidade){
-    $estoque[] = [
-        'nome' => $nome
-    ];
-}
-
-// Função para exibir o menu e obter a escolha do usuário
-function exibirMenu(){
+function exibirMenu() {
+    
     echo "\n";
     echo "Escolha uma das opções abaixo:\n";
     echo "1. Adicionar um produto 1\n";
@@ -43,10 +13,103 @@ function exibirMenu(){
     return $opcao;
 }
 
+function adicionarProduto(&$estoque, $codigo, $nome, $tamanho, $cor, $quantidade) {
+    
+    $estoque[] = [
+        'codigo' => $codigo,
+        'nome' => $nome,
+        'tamanho' => $tamanho,
+        'cor' => $cor,
+        'quantidade' => $quantidade
+    ];
+}
+
+function venderProduto(&$estoque, $codigo, $quantidade) {
+
+    $produtoEncontrado = false;
+
+    foreach($estoque as $key => &$produto) { 
+
+        if($produto['codigo'] == $codigo) {
+
+            $produtoEncontrado = true;
+
+            if($quantidade > $produto['quantidade']){
+                
+                echo"\nVenda de " . $produto['nome'] . " não realizada.\nExiste apenas " .  $produto['quantidade'] . " unidades disponiveis.\n";
+
+            return;
+            }
+
+            $produto['quantidade'] -= $quantidade;
+
+            if ($produto['quantidade'] == 0) {
+                
+                echo "\nVenda de " . $quantidade . " " . $produto['nome'] . " realizada com sucesso.";
+                echo "\nProduto: " . $produto['nome'] . " esgotado.\n";
+                unset($estoque[$key]);
+
+            } else {
+
+                echo "\nVenda de " . $quantidade . " " . $produto['nome'] . " realizada com sucesso.\n";
+                echo "Quantidade restante: " . $produto['quantidade'] . "\n";
+            }
+            return;
+        }
+    } 
+
+    if($produtoEncontrado == false) {
+
+        echo "\nCódigo do produto inválido, por favor tente novamente.\n";
+    }   
+    return;
+}
+
+function verificarEstoque(&$estoque, $codigo) {
+    
+    $produtoEncontrado = false;
+
+    foreach($estoque as $produto) {
+
+        if($produto['codigo'] == $codigo) {
+
+            echo "\nO produto: " . $produto['nome'] . " está disponível." . "\nQuantidade: " . $produto['quantidade'] . ".\n";
+            $produtoEncontrado = true;
+            return;
+        }
+    }
+
+    if ($produtoEncontrado == false) {
+
+        echo "\nProduto não está disponível.\n";
+    }
+}
+
+function listarEstoque(&$estoque) {
+    
+    if(empty($estoque)){
+        echo "Estoque vazio!\n";
+        return;
+    }
+    
+    foreach($estoque as $produto){
+
+        echo "\nCódigo: " . $produto['codigo'] . "\nNome: " . $produto['nome'] . "\nTamanho: ". $produto['tamanho'] . "\nCor: ". $produto['cor'] . "\nQuantidade: ". $produto['quantidade'] . "\n\n";
+    }
+}
+
+$estoque[] = [
+    'codigo' => 1,
+    'nome' => 'Camisa Polo',
+    'tamanho' => 'P',
+    'cor' => 'Branca',
+    'quantidade' => 5
+];
+
 while (true) {
-
+    
     $opcao = exibirMenu();
-
+   
     switch ($opcao) {
         case 1:
             echo "Adicionar um produto\n";
@@ -58,12 +121,15 @@ while (true) {
             adicionarProduto($estoque, $codigo, $nome, $tamanho, $cor, $quantidade);
             break;
         case 2:
-            echo "Remover um produto\n";
-            // Implemente aqui o código para a opção 2
+            echo "Vender um produto\n";
+            $codigo = readline("Digite o código do produto para venda: ");
+            $quantidade = readline("Digite a quantidade para venda: ");
+            venderProduto($estoque, $codigo, $quantidade);
             break;
         case 3:
             echo "Verificar Estoque\n";
-            // Implemente aqui o código para a opção 3
+            $codigo = readline("Digite o códgigo do produto para verificar: ");
+            verificarEstoque($estoque, $codigo);
             break;
         case 4:
             echo "Listar o Estoque\n";
@@ -71,9 +137,10 @@ while (true) {
             break;
         case 5:
             echo "Saindo...\n";
-            exit; // Sai do loop e encerra o script
+            exit;
         default:
-            echo "Opção inválida, por favor tente novamente.\n";
+            echo "\nOpção inválida, por favor tente novamente.\n";
             break;
     }
 }
+?>
