60d08a044f223b56fb40f43b75580ccbb04cddb6
João Pedro Vicente Pansiere <ojp027@gmail.com>
2024-07-05 06:02:52 -0300

    Adicionando mais deveres

---
+++ SQL-MYSQL/7 - populandoPRODUTOS.sql
@@ -0,0 +1,19 @@
+USE gestao_produtos;
+
+INSERT INTO categorias (`nome_categoria`,`created_at`)
+VALUES ('BLUSA', NOW()), ('PESOS', NOW()), ('CHINELO', NOW());
+
+INSERT INTO unidades_medidas (`nome_unidade_medida`,`created_at`)
+VALUES ('43', NOW()), ('KG', NOW()), ('GG', NOW());
+
+INSERT INTO fabricantes (`nome_fabricante`,`created_at`)
+VALUES ('PHP-maker', NOW()), ('fabi-codes', NOW()), ('TrabalhaEConfia', NOW());
+
+INSERT INTO fornecedores (`nome_fornecedor`,`created_at`)
+VALUES ('ImportaBR', NOW()), ('FreteRapido', NOW()), ('0-taxas', NOW());
+
+
+INSERT INTO produtos (nome ,sku ,descricao, categoria, preco ,unidade_medida, peso, quantidade_estoque, fabricante, fornecedor, create_at)
+VALUES ('Camisa_Polo', '123', 'Cor_preta', 1, 50, 3, 5, 30, 3, 2, NOW()),
+('Havainas', '1234', 'Cor_branca', 3, 25, 1, 5, 20, 3, 3, NOW()),
+('Anilha', '12342', 'Cor_preta', 2, 35, 2, 40, 50, 1, 1, NOW());
---
+++ SQL-MYSQL/8 - produtos_frabricanteX.sql
@@ -0,0 +1,1 @@
+USE gestao_produtos;
