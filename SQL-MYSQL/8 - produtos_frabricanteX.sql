da9595f5933fe6f16f9bb0c126070dacae8291c7
João Pedro Vicente Pansiere <ojp027@gmail.com>
2024-07-05 07:28:25 -0300

    update

--- SQL-MYSQL/8 - produtos_frabricanteX.sql
+++ SQL-MYSQL/8 - produtos_frabricanteX.sql
@@ -1,1 +1,6 @@
 USE gestao_produtos;
+
+SELECT f.nome_fabricante as fabricante, `nome`, `sku`, `descricao`, `categoria`, `preco`, `unidade_medida`, `peso`, `quantidade_estoque`, `fornecedor`, `create_at`, `update_at`
+FROM produtos p
+INNER JOIN fabricantes f ON p.fabricante = f.id
+WHERE p.fabricante = 3;
