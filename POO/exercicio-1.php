4fafc89de71d300035bc588dabee46e0ed61797c
João Pedro V. Pansiere <ojp027@gmail.com>
2024-07-18 20:26:52 -0300

    Exercício POO 18-07-24

---
+++ POO/exercicio-1.php
@@ -0,0 +1,28 @@
+<?php
+
+class Carro
+{
+    private $marca;
+    private $modelo;
+    private $ano;
+    private $acelerando = false;
+
+    public function __construct(string $marca, string $modelo, int $ano)
+    {
+        $this->marca = $marca;
+        $this->modelo = $modelo;
+        $this->ano = $ano;
+    }
+
+    public function acelerar(): string
+    {
+        $this->$acelerando = true;
+        return 'Acelerando o carro.'
+    }
+
+    public function frear(): string
+    {
+        $this->$acelerando = false;
+        return 'Freando o carro.'
+    }
+}
