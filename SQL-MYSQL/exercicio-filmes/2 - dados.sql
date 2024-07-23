d104b03255f3dc05c3307a7c972f572f75f271d4
JP <ojp027@gmail.com>
2024-07-23 00:00:54 -0300

    dados.sql

---
+++ SQL-MYSQL/exercicio-filmes/2 - dados.sql
@@ -0,0 +1,18 @@
+INSERT INTO codifica.filmes
+VALUES (
+    'Divertida-Mente',
+    'Inside Out',
+    DEFAULT,
+    'Animacao, Aventura, Comedia',
+    2015,
+    8.1,
+    95
+), (
+    'Dune - Duna: Parte Dois',
+    'Dune: Part Two',
+    14,
+    'Ação, Aventura, Drama',
+    2024,
+    8.6,
+    166
+);
