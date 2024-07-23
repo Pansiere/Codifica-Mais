330fa0b3a14c2e51d2022497d248b233d4042e97
JP <ojp027@gmail.com>
2024-07-23 00:56:26 -0300

    Feito exercicio de inserir dados

--- SQL-MYSQL/exercicio-filmes/1 - estrutura.sql
+++ SQL-MYSQL/exercicio-filmes/1 - estrutura.sql
@@ -1,11 +1,13 @@
 CREATE DATABASE IF NOT EXISTS codifica;
 
 CREATE TABLE IF NOT EXISTS `codifica`.`filmes` (
+    `id` INT NOT NULL AUTO_INCREMENT,
     `titulo` VARCHAR(255) NOT NULL ,
     `titulo_original` VARCHAR(255) NOT NULL ,
     `classificacao` INT NOT NULL DEFAULT '0' ,
-    `categoria` VARCHAR(255) NOT NULL ,
+    `categoria` TEXT NOT NULL ,
     `ano_lancamento` INT NOT NULL ,
-    `nota` FLOAT(3,2) NOT NULL ,
-    `duracao` INT NOT NULL
+    `nota` DECIMAL(3,1) NOT NULL ,
+    `duracao` INT NOT NULL,
+    PRIMARY KEY (`id`)
     ); 
