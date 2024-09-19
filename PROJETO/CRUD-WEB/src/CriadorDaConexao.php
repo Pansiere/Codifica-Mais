9d3bb17cfab49cec24c149659056c57858b27596
João Pedro V. Pansiere <ojp027@gmail.com>
2024-09-19 20:07:56 -0300

    Adicionando o try e catch

--- PROJETO/CRUD-WEB/src/CriadorDaConexao.php
+++ PROJETO/CRUD-WEB/src/CriadorDaConexao.php
@@ -19,7 +19,7 @@ class CriadorDaConexao
 
             return $connection;
         } catch (\PDOException $e) {
-            echo $e->getMessage();
+            die($e->getMessage());
         }
     }
 }
